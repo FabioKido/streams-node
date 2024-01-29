097b4226b2df5ba281099570f705a199a4ab581c
Fábio Henrique <38080922+FabioKido@users.noreply.github.com>
2024-01-28 23:43:46 -0300

    add streams-v2 example

---
+++ streams-v2.js
@@ -0,0 +1,37 @@
+// Streams
+
+const crypto = require('crypto')
+const { Readable, Writable, Transform } = require('stream')
+
+// Readable
+const input = new Readable({
+    read() {
+        for(let i = 0; i < 10000; i++) {
+            this.push(crypto.randomUUID())
+        }
+        this.push(null)
+    }
+})
+
+// Transform
+const toUpperCase = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, chunk.toString().toUpperCase())
+    }
+})
+
+const addText = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, chunk.toString() + " Trasnformado")
+    }
+})
+
+// Writable
+const output = new Writable({
+    write(chunk, encoding, callback) {
+        console.log(chunk.toString())
+        callback()
+    }
+})
+
+input.pipe(toUpperCase).pipe(addText).pipe(output);
